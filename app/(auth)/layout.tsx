fd31dcd2f3a05578bd7ed1744992f705f804bd8d
Dejvi31 <dejviepadejvi@gmail.com>
2025-05-02 11:01:23 +0200

    small change

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
-import { isAuthenticated } from '@/lib/actions/auth.auction';
-import { redirect } from 'next/navigation';
+// import { isAuthenticated } from '@/lib/actions/auth.auction';
+// import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react'
 
 const AuthLayout = async ({children}: {children: ReactNode}) => {
-  const isUserAuthenticated = await isAuthenticated();
+  // const isUserAuthenticated = await isAuthenticated();
  
-   if(!isUserAuthenticated) redirect('/')
+  //  if(!isUserAuthenticated) redirect('/')
  
   return (
     <div className='auth-layout'>{children}</div>
